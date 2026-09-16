f97ab4d77bd4dc160bc036488275710c63939a95
juanjosebazan21-star <juanjosebazan21@gmail.com>
2026-09-16 08:53:32 -0300

    web en 2.7.0

--- lead.js
+++ lead.js
@@ -60,7 +60,7 @@
   origenCampana();   /* captura al cargar, no al enviar */
 
   var DEXIAE_LINKS = {
-    installer: 'https://github.com/dexiaesoporte-create/dexiae-releases/releases/download/v2.5.18/DEXIAE_Setup_V2.5.18.exe',
+    installer: 'https://github.com/dexiaesoporte-create/dexiae-releases/releases/download/v2.7.0/DEXIAE_Setup_V2.7.0.exe',
     whatsapp: 'https://wa.me/5493516574188?text=Hola!%20Quiero%20activar%20DEXIAE'
   };
   var MP_LINKS = {
